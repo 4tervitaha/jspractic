d9e51a4dab61ede185c04f04308890c9c3100270
vitaliy <4tervitaha@gmail.com>
2022-02-22 01:27:29 +0200

    commit

--- Untitled-1.js
+++ Untitled-1.js
@@ -1,1 +1,73 @@
-"use strict"
+'use strict'
+
+function grafitiDisplay(number, icon){
+
+    let str =' ';
+
+    for(let i = 0; i <= number; i++){
+    for(let j = 0; j <= number ; j++){
+        if( i === 0 || i === number || j === 0 || j === number || i === j){ 
+                str += icon ;
+        } else{
+                str += ' ';
+            }
+    
+            
+        }
+
+    str +='\n';
+    }
+return str;
+}
+console.log(grafitiDisplay(10,'*'));
+
+
+
+
+
+function pUgol(size, simvol){
+
+    let stp =' ';
+
+    for(let i = 0; i <= size; i++){
+    for(let j = 0; j <= size ; j++){
+        if( i === size ||i >=j ||j === 0 ||  i === j){ 
+                stp += simvol ;
+        } else{
+                stp += ' ';
+            }
+    
+            
+        }
+
+    stp +='\n';
+    }
+return stp;
+}
+console.log(pUgol(10,'*'));
+
+
+
+
+
+////  должен был быть равнобедренный треугольник
+function ugol(x,y, sim){
+
+    let stk =' ';
+
+    for(let i = 0; i <= y; i++){
+    for(let j = 0; j <= x ; j++){
+        if( )){
+                stk += sim ;
+        } else{
+                stk += ' ';
+            }
+    
+            
+        }
+
+    stk +='\n';
+    }
+return stk;
+}
+console.log(ugol(4,7,'*'));
